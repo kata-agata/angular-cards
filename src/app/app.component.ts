f2e431edc212462ec1d691dcccb781f81f46abbb
kata-agata <agata.k.jazdzewska@gmail.com>
2020-11-18 13:41:02 +0100

    card component property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-cards';
+
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today'
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'moutainlover',
+      content: 'Here is an image of moutain'
+    },
+    {
+      title: 'Mountain Biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biking2342',
+      content: 'I did some biking today'
+    }
+  ]
 }
